4a13010c513d0f6e50fbbb8c4dcdbf3f6e6b2f10
kuma-24 <surei.mikurio2525@gmail.com>
2021-03-24 11:33:35 +0900

    各ページ第一段階整形

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
+  
+  with_options presence: true do
+    validates :name
+    validates :password, length: {minimum: 6}
+  end
 end
