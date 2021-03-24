c20f160efb1a51d467aca19e78d49e3e7a555b4b
kuma-24 <surei.mikurio2525@gmail.com>
2021-03-24 17:33:38 +0900

    rubocop修正

--- src/app/controllers/users/passwords_controller.rb
+++ src/app/controllers/users/passwords_controller.rb
@@ -1,9 +1,3 @@
 class Users::PasswordsController < Devise::PasswordsController
   before_action :ensure_normal_user, only: :create
-
-  def ensure_normal_user
-    if params[:user][:email].downcase == 'guest@example.com'
-      redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
-    end
-  end
 end
