a68ca399eff3e2da78359b92668e4aa383a8b2ae
kuma-24 <surei.mikurio2525@gmail.com>
2021-03-24 17:15:40 +0900

    ゲストログイン実装

--- src/app/models/user.rb
+++ src/app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com', name: 'ゲスト') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
