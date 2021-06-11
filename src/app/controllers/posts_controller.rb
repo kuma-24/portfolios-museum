306b0ee48b833138566120eb2bf28186b63f2d21
kuma-24 <surei.mikurio2525@gmail.com>
2021-06-11 21:44:21 +0900

    ページネーション実装

--- src/app/controllers/posts_controller.rb
+++ src/app/controllers/posts_controller.rb
@@ -1,21 +1,16 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all.includes(:user, :likes, :post_arbitrary).order(created_at: :desc)
+    @posts = Post.all.includes(:user, :likes, :post_arbitrary).page(params[:page]).order(created_at: :desc).per(8)
     @posts_like_order = Post.includes(:liked_users, :user, :post_arbitrary).limit(3).sort { |a, b| b.liked_users.size <=> a.liked_users.size }
     @posts_create_order = Post.all.includes(:liked_users, :user, :post_arbitrary).limit(3).order(created_at: :desc)
   end
-
-  def show
-    @post = Post.includes(:user, :likes, :post_arbitrary, :comments).find(params[:id])
-  end
-
+  
   def new
     @post = PostForm.new
   end
 
   def create
     @post = PostForm.new(post_params)
-
     if @post.valid?
       @post.save
       redirect_to root_path
@@ -24,9 +19,32 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+    @post = Post.includes(:user, :likes, :post_arbitrary, :comments).find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy!
+    redirect_to("/")
+  end
+
   def search
-    @posts = Post.search(params[:keyword])
-    @posts = Post.sort_category(params[:category_keyword])
+    @posts = Post.search(params[:keyword]).page(params[:page]).per(8)
+    @posts = Post.sort_category(params[:category_keyword]).page(params[:page]).per(8)
     @posts_like_order = Post.includes(:liked_users, :user, :post_arbitrary).limit(3).sort { |a, b| b.liked_users.size <=> a.liked_users.size }
     @posts_create_order = Post.all.includes(:liked_users, :user, :post_arbitrary).limit(3).order(created_at: :desc)
   end
--- src/config/routes.rb
+++ src/config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :users
-  resources :posts, only: %i[index show new create destroy] do
+  resources :posts do
     get :search, on: :collection
   end
 
