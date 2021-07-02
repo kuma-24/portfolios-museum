389ae3a6ada8b3ce4d6b64692cb819525a2c1b2b
kuma-24 <surei.mikurio2525@gmail.com>
2021-07-02 18:37:27 +0900

    rubocop修正

--- src/app/helpers/application_helper.rb
+++ src/app/helpers/application_helper.rb
@@ -1,15 +1,5 @@
 module ApplicationHelper
-  require "uri"
-
   def simple_time(time)
     time.strftime('%Y-%m-%d %H:%M ')
-  end
-
-  def text_url_to_link(text)
-    URI.extract(text, ["http", "https"]).uniq.each do |url|
-      sub_text = ""
-      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
-    end
-    return text
   end
 end
