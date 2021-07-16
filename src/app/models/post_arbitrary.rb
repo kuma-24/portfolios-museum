88ee7b7bf74de10d3bdc9c9f38e16d50da475d10
kuma-24 <surei.mikurio2525@gmail.com>
2021-07-16 12:57:30 +0900

    ゲストログイン不具合修正

--- src/app/models/post_arbitrary.rb
+++ src/app/models/post_arbitrary.rb
@@ -2,5 +2,6 @@ class PostArbitrary < ApplicationRecord
   mount_uploader :thumbnail_img, ThumbnailImgUploader
   belongs_to :post
 
-  validates :thumbnail_img, presence: true
+  validates :thumbnail_img, presence: true, on: :update
+  validates :site_url, presence: true, on: :update
 end
